b9c1d089861daa690791460607ae5c506c613473
Israel Adefidipe <israeladefidipe@gmail.com>
2021-05-29 23:39:06 +0100

    fixed bug caused by not initializing scores array

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 'use strict';
 
-
 // Selcting elements
-
-
 
 // there are two ways to select an element by id, on is using the query selector and add #idName
 const score0El = document.querySelector('#score--0');
@@ -18,87 +15,75 @@ const curent1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-
 // Starting condition
-score0El.textContent= 0;
-score1El.textContent =0;
+score0El.textContent = 0;
+score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 // global variable
-const scores = [0,0];
-let currentScore =0;
-let activePlayer =0;
+let scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
-
-// switch player function
-const switchPlayer = function(){
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
+// TODO: switch player function
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
 };
 
-
 // addeing event listener to roll dice btn
-btnRoll.addEventListener('click', function(){
+btnRoll.addEventListener('click', function () {
+  // block variable to generate random dice roll number
+  const dice = Math.trunc(Math.random() * 6) + 1;
 
-    
-    // block variable to generate random dice roll number
-    const dice= Math.trunc(Math.random()*6)+1;
+  // display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 
-    // display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+  // check if dice = 1
 
-    // check if dice = 1
+  if (dice !== 1) {
+    currentScore += dice;
 
-    if (dice !==1){
-        currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    // switch to next player
+    switchPlayer();
+  }
+});
 
-        document.getElementById(`current--${activePlayer}`).textContent=currentScore
-        
-    }else{
-        // switch to next player
-        switchPlayer();
-        
-        
-    }
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
 
+  if (scores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.toggle('player--winner');
+  } else {
+    switchPlayer();
+  }
+});
 
-})
-
-btnHold.addEventListener('click', function(){
-    scores[activePlayer] += currentScore
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-    
-    if (scores[activePlayer] >=100){
-        document.querySelector(`.player--${activePlayer}`).classList.toggle('player--winner' );
-        
-    }else{
-        switchPlayer();
-
-
-    }
-
-
-})
-
-// start new game 
-btnNew.addEventListener('click', function(){
-    currentScore = 0;
-    activePlayer = 0;
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
-    player0El.classList.remove('player--winner');
-    player1El.classList.remove('player--winner');
-    score1El.textContent =currentScore;
-    score0El.textContent = currentScore;
-    curent0El.textContent=currentScore;
-    curent1El.textContent = currentScore;
-
-})
-
-
-
+// start new game
+btnNew.addEventListener('click', function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores=[0,0];
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  score1El.textContent = currentScore;
+  score0El.textContent = currentScore;
+  curent0El.textContent = currentScore;
+  curent1El.textContent = currentScore;
+  diceEl.classList.add('hidden');
+});
 
