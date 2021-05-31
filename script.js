0de415aa49ef1d636079dedc608015f3535d0eb7
Israel Adefidipe <israeladefidipe@gmail.com>
2021-05-31 17:42:16 +0100

    fixed game state, gsme works well now

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Selcting elements
+// TODO: Selcting elements an storing them in their own variable to make code more dry
 
 // there are two ways to select an element by id, on is using the query selector and add #idName
 const score0El = document.querySelector('#score--0');
@@ -20,59 +20,73 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+
 // global variable
 let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true; // state variable 
 
 // TODO: switch player function
 const switchPlayer = function () {
-  currentScore = 0;
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
+  currentScore = 0; // reset score in the program
+  document.getElementById(`current--${activePlayer}`).textContent = 0; // rest score in the user interface
+  document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+  activePlayer = activePlayer === 0 ? 1 : 0; //conditional statement to change active player value 
+  document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
 };
 
-// addeing event listener to roll dice btn
+// Todo: adding event listener to roll dice btn
 btnRoll.addEventListener('click', function () {
-  // block variable to generate random dice roll number
-  const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+  if(playing){
+    // TODO: generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // check if dice = 1
+    // TODO: dice to diplay randomly generated between 1-6
+    diceEl.classList.remove('hidden'); // remove hidden class to display the dice img
+    diceEl.src = `dice-${dice}.png`;
 
-  if (dice !== 1) {
-    currentScore += dice;
+    // TODO: check if dice rolled 1, if true switch players
 
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-  } else {
-    // switch to next player
-    switchPlayer();
+    if (dice !== 1) {
+      currentScore += dice;
+
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // todo: switch to next player
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  scores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+  if(playing){
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer]; // add current score to the player score on the interface
 
-  if (scores[activePlayer] >= 100) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.toggle('player--winner');
-  } else {
-    switchPlayer();
+    if (scores[activePlayer] >= 20) {
+      playing = false; //change state of the game
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.toggle('player--winner');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+
+      diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
   }
 });
 
-// start new game
+// TODO: start new game, initializing all states 
 btnNew.addEventListener('click', function () {
+  playing=true
   currentScore = 0;
   activePlayer = 0;
   scores=[0,0];
